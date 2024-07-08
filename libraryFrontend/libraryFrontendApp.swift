0959555427cde1ddce1e3b75272d52d3f2ae7c0d
bechir105 <mohamedbechir.kefi@esprit.tn>
2024-07-08 10:42:24 +0100

    Initial Commit

---
+++ libraryFrontend/libraryFrontendApp.swift
@@ -0,0 +1,17 @@
+//
+//  libraryFrontendApp.swift
+//  libraryFrontend
+//
+//  Created by Bechir Kefi on 8/7/2024.
+//
+
+import SwiftUI
+
+@main
+struct libraryFrontendApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
